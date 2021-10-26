866353e4494c8ec8dfa87c6d0fc14b52eb4f655b
Nikolai Saganenko <9952728@gmail.com>
2021-10-26 13:53:59 +0300

    Add CIFilters

--- ImageFilter/ViewController.swift
+++ ImageFilter/ViewController.swift
@@ -9,9 +9,6 @@ import UIKit
 
 class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
     @IBOutlet weak var imageView: UIImageView!
- 
-    @IBAction func filterButtonPressed(_ sender: UIButton) {
-    }
     
     @IBAction func chooseButtonPressed(_ sender: UIBarButtonItem) {
         
@@ -25,9 +22,59 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
     }
     
     func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        navigationController?.dismiss(animated: true, completion: nil)
         if let image = info[UIImagePickerController.InfoKey.originalImage] as? UIImage {
             imageView.image = image
+            selectedImage = image
         }
     }
+    
+    var selectedImage: UIImage?
+    
+    @IBAction func filterButtonPressed(_ sender: UIButton) {
+        if let image = selectedImage {
+            let filter = CIFilter(name: "CISepiaTone")
+            let context = CIContext()
+            filter?.setValue(0.7, forKey: kCIInputIntensityKey)
+            filter?.setValue(CIImage(image: image), forKey: kCIInputImageKey)
+            if let output = filter?.outputImage {
+                imageView.image = UIImage(cgImage: context.createCGImage(output, from: output.extent)!)
+            } else {
+                print("No output")
+            }
+        } else {
+            print("No selectedImage")
+        }
+    }
+    
+    @IBAction func noir(_ sender: UIButton) {
+        if let image = selectedImage {
+            let filter = CIFilter(name: "CIPhotoEffectNoir")
+            let context = CIContext()
+            filter?.setValue(CIImage(image: image), forKey: kCIInputImageKey)
+            if let output = filter?.outputImage {
+                imageView.image = UIImage(cgImage: context.createCGImage(output, from: output.extent)!)
+            } else {
+                print("No output")
+            }
+        } else {
+            print("No selectedImage")
+        }
+    }
+    @IBAction func vintage(_ sender: UIButton) {
+        if let image = selectedImage {
+            let filter = CIFilter(name: "CIPhotoEffectProcess")
+            let context = CIContext()
+            filter?.setValue(CIImage(image: image), forKey: kCIInputImageKey)
+            if let output = filter?.outputImage {
+                imageView.image = UIImage(cgImage: context.createCGImage(output, from: output.extent)!)
+            } else {
+                print("No output")
+            }
+        } else {
+            print("No selectedImage")
+        }
+    }
+    
 }
 
